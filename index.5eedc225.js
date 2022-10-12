5b93d8f6070f8127f455a8e7bc5a26fc1fd2bcc8
nulevii <cherniugov@gmail.com>
2022-10-12 12:03:51 +0000

    Deploying to gh-pages from @ nulevii/IT_Pro_Sto_Project@61e703d5079531eac3c0476db8ffa4e53b28bb0d 🚀

---
+++ index.5eedc225.js
@@ -0,0 +1,2 @@
+new Swiper(".gallery-slider",{slidesPerView:1,spaceBetween:30,centeredSlides:!0,autoplay:{delay:5e3,disableOnInteraction:!1},loop:!0}),new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(e,n){return'<span class="'+n+'">'+(e+1)+"</span>"}}});
+//# sourceMappingURL=index.5eedc225.js.map
