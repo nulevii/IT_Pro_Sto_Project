61e703d5079531eac3c0476db8ffa4e53b28bb0d
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-10-12 15:03:00 +0300

    Fix index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,13 @@ new Swiper('.gallery-slider', {
   },
   loop: true,
 });
+
+new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});
