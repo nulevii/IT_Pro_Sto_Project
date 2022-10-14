828dc580b7fc4528309ed5830123342d912de0b2
nulevii <cherniugov@gmail.com>
2022-10-14 07:26:21 +0000

    Deploying to gh-pages from @ nulevii/IT_Pro_Sto_Project@2a2523cd99a2a9b30b1d4166707894db3d49aa38 🚀

--- index.ce30757f.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".gallery-slider",{slidesPerView:1,spaceBetween:30,centeredSlides:!0,autoplay:{delay:5e3,disableOnInteraction:!1},loop:!0}),new Swiper(".clients__slider",{centeredSlides:!0,slidesPerView:1,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}}),AOS.init();
-//# sourceMappingURL=index.ce30757f.js.map
